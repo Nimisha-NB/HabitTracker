3ea63eaf67177cc3951d6a7816aa61a35ea6e869
nimisha <nbnimisha04@gmail.com>
2024-07-08 23:48:11 +0530

    Auth login and sigup pages + backend logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // pages and components
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 
@@ -14,6 +16,14 @@ function App() {
           <Route
           path="/"
           element={<Home/>}
+          />
+          <Route
+          path="/login"
+          element={<Login/>}
+          />
+          <Route
+          path="/signup"
+          element={<Signup/>}
           />
         </Routes>
       </div>
